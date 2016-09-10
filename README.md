fc90e3396f97a8622e0210e3711c38a18f29e84a
francisco-campos <franciscojavier.c.c@gmail.com>
2016-09-10 01:16:15 -0300

    proyecto base

---
+++ README.md
@@ -0,0 +1,4 @@
+#RSPEC Example
+
+<h2>Proyecto básico con ejemplo de test utilizando RSPEC</h2>
+
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+    describe "get index" do
+        it "has a 200 status code" do 
+            get :index
+            expect(response.status).to eq(200)
+        end
+
+    end
+
+    describe "get detalle" do
+        it "has a 200 status code" do
+            get :detalle
+            expect(response.status).to eq(200)
+        end
+    end
+end
